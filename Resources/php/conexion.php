65b339a827ab7ebf5f137b3b356af3765c9614fe
saelric <saelric@gmail.com>
2017-08-03 10:59:37 -0500

    holi

--- Resources/php/conexion.php
+++ Resources/php/conexion.php
@@ -1,8 +1,8 @@
 <?PHP
-error_reporting(0);
+//error_reporting(0);
 
 session_start();
-function  conect($host = "localhost:3306", $user = "root", $psw = "", $db = "ametriz"){
+function  conect($host = "localhost", $user = "root", $psw = "", $db = "ametriz"){
 	$con = mysqli_connect($host,$user,$psw, $db) or die ("Error de la conexión MySQL");
     mysqli_set_charset($con,'utf8');
 	if (!$con){
